999f7338180ea743fba5d47dd83a20738baa1027
quanwangniuniu <1606327882@qq.com>
2024-06-26 22:05:28 +0800

    增加footerControl模块

---
+++ UserControl/FooterControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Niuniumama.UserControl
+{
+    public partial class FooterControl : System.Windows.Controls.UserControl
+    {
+        public FooterControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        
+        private void OpenFacebook(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://www.facebook.com");
+        }
+
+        private void OpenTwitter(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://www.twitter.com");
+        }
+
+        private void OpenLinkedIn(object sender, RoutedEventArgs e)
+        {
+            OpenUrl("https://www.linkedin.com");
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show($"Unable to open link: {ex.Message}");
+            }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+    }
+}
